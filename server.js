f0b86dd4dca22e76ff6337a4598e1b89d12a5afd
Mati <mati.carballo91@gmail.com>
2016-09-06 17:46:47 -0300

    mensaje

--- server.js
+++ server.js
@@ -8,18 +8,83 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars',handlebars.engine);
 app.set('view engine', 'handlebars');
 
-//Importo mas cosas dps
+//Importo mas cosas 
+
+app.use(require('body-parser').urlencoded({extended: true}));
+
+var formidable = require('formidable');
+
+var credentials = require('./credentials.js');
+app.use(require('cookie-parser')(credentials.cookieSecret));
 
 app.set('port',process.env.PORT || 3000);
 
 
 app.use(express.static(__dirname + '/public'));
 
+//Metodo get para ir a la ruta web del handlebars
+app.get('/about', function(request, response) {
+	response.render('about');
+});
 
+app.get('/contact', function(request, response) {
+	response.render('contact',{csrf :'csrf token'});
+});
+
+app.get('/gracias', function(request, response) {
+	response.render('gracias');
+});
+
+app.post('/process',function(request,response){
+	console.log('Form '+ request.query.form);
+	console.log('Token '+ request.body._csrf);
+	console.log('Email '+ request.body.email);
+	console.log('Preguntas '+ request.body.ques);
+	response.redirect(303, '/gracias');
+});
+
+app.get('/file-upload', function(request, response) {
+	var now = new Date();
+	response.render('file-upload',{
+		year: now.getFullYear(),
+		month: now.getMonth()
+	});
+});
+//Si me pasan de una por url la data lo manejo distinto
+app.post('/file-upload/:year/:month', function(request, response) {
+	var form = new formidable.IncomingForm();
+	form.parse(request,function(err,fields,file){
+		if(err)
+			return response.redirect(303,'/505');
+		console.log('Received File');
+		console.log(file);
+		response.redirect(303,'/gracias');
+	
+	});
+});
 
 app.get('/', function(request, response) {
 	response.render('home');
 });
+
+app.use(function(request, response,next) {
+	console.log("Looking for URL: "+request.url);
+	next();
+});
+//ERRORES 404 Y 500
+app.use(function(request, response,next) {
+	response.type('text/html');
+	response.status(404);
+	response.render('404');
+	
+});
+app.use(function(err,request, response,next) {
+	console.error(err.stack);
+	response.status(500);
+	response.render('500');
+	
+});
+
 
 
 app.listen(app.get('port'), function(){

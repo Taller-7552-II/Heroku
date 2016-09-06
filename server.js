3edbfe70a37988da5f070cdca6b7a782a073c903
Mati <mati.carballo91@gmail.com>
2016-09-05 23:27:18 -0300

    julycomit

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 var express = require('express');
 var app = express();
 
+app.disable('x-powered-by');
+
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars',handlebars.engine);
+app.set('view engine', 'handlebars');
+
+//Importo mas cosas dps
+
 app.set('port',process.env.PORT || 3000);
 
+
+app.use(express.static(__dirname + '/public'));
+
+
+
 app.get('/', function(request, response) {
-	response.send("super");
+	response.render('home');
 });
 
-app.get('/july', function(request, response) {
-	response.send("epic");
-});
 
 app.listen(app.get('port'), function(){
 	console.log('Jobify working, kill it with Ctrl-C');

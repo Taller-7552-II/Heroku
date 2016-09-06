25c6ef5c109f89acf97d700aa2bc932e5175e429
Mati <mati.carballo91@gmail.com>
2016-09-05 22:26:52 -0300

    segundo comit

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var http= require('http');
+var express = require('express');
+var app = express();
 
 var server = http.createServer(function(req,res){
 	res.writeHead(200, {'Content-Type': 'text/html'});
@@ -6,3 +8,7 @@ var server = http.createServer(function(req,res){
 });
 var port = Number(process.env.PORT || 3000);
 server.listen(port);
+
+app.get('/july', function(request, response) {
+	response.send("epic");
+}

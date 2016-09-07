e03c6fd41f55c024102ddfff7cb01d99d0f3432e
Mati <mati.carballo91@gmail.com>
2016-09-06 23:04:38 -0300

    locuraton

--- server.js
+++ server.js
@@ -36,13 +36,14 @@ app.get('/contact', function(request, response) {
 var pg = require('pg');
 
 app.get('/db', function (request, response) {
+	console.log(process.env.DATABASE_URL);
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+    client.query('SELECT * FROM usuarios', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.render('pages/db', {results: result.rows} ); }
+       { response.render('/db', {results: result.rows} ); }
     });
   });
 });

3fc57f14591da0fdc1a60c172809388cff6d840e
Matias <matisyo@users.noreply.github.com>
2016-10-06 12:26:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ app.get('/usuarios/:user', function (request, response) {
   	if (err)
        { console.error('Rompio loco',err);}
     var num = request.url.toString().substring(10, request.url.toString().length);
-    client.query('SELECT * FROM usuarios where usuario = '+num , function(err, result) {
+    client.query('SELECT * FROM usuarios where usuario = fede' , function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }

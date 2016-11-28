a4126b1896dd655a7569b3c84ac6387a35afcde1
Matias <matisyo@users.noreply.github.com>
2016-11-28 17:52:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -133,6 +133,34 @@ app.post('/categories', function (request, response) {
 });
 
 
+//GET CATEGORY POSITIONS
+var pg = require('pg');
+
+var connectionString = "postgres://nlmbufkijzqmqs:2BESGXz_KTUisRfo4MmdoJBNid@ec2-54-235-254-199.compute-1.amazonaws.com:5432/d36ea1inur7hrd";
+app.get('/categories', function (request, response) {
+
+  pg.defaults.ssl = true;
+  pg.connect(connectionString, function(err, client, done) {
+  	if (err)
+       { console.error('Rompio loco',err);}
+    var num = request.url.toString().substring(26, request.url.toString().length);
+    var comilla = '\'';
+    client.query('SELECT * FROM categories ' , function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       {
+	       
+	       var rta = "{ \"categories\": "+JSON.stringify(result.rows);
+	       rta = rta+",\"metadata\": { \"version\": \"0.1\",\"count\": "+JSON.stringify(result.rows.length)+"}}";
+	         rta = rta.replace(/\\/g , "");
+	         response.write(rta); 
+       		response.end(); }
+    });
+  });
+});
+
 //TODOS LOS  SKILLS
 //PUT JOB POSITIONS
 var pg = require('pg');

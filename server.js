cebb039ebc30e4546b9e120144dd3616613c4ca3
Matias <matisyo@users.noreply.github.com>
2016-11-30 22:02:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ var credentials = require('./credentials.js');
 app.use(require('cookie-parser')(credentials.cookieSecret));
 
 app.set('port',process.env.PORT || 3000);
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(express.static(__dirname + '/public'));
 
